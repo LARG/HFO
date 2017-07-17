661eea7509d760bc40e41e8adc91c0ccca6c78fb
drallensmith <drallensmith@users.noreply.github.com>
2017-07-17 12:13:40 -0500

    Fix for defense_python; checking 5000 episodes for comparison

--- example/defense_python_long_2v2.sh
+++ example/defense_python_long_2v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./bin/HFO --offense-npcs=3 --defense-agents=1 --defense-npcs=2 --trials 20 --headless &
+./bin/HFO --offense-npcs=2 --defense-agents=1 --defense-npcs=1 --trials 5000 --headless --seed 1500310928 --no-logging &
 sleep 15
 ./example/hand_coded_defense_agent.py &> agent1.txt &
 sleep 5
