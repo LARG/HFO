4be05faed9ed4d354fc06293e816ad493fab092a
drallensmith <drallensmith@users.noreply.github.com>
2017-07-18 15:15:24 -0500

    Hfo logging

--- example/defense_python_long_2v2.sh
+++ example/defense_python_long_2v2.sh
@@ -2,7 +2,7 @@
 
 # Change to a different seed for different experiments!
 
-./bin/HFO --offense-npcs=2 --defense-agents=1 --defense-npcs=1 --trials 5000 --headless --seed 1500348586 --no-logging &
+./bin/HFO --offense-npcs=2 --defense-agents=1 --defense-npcs=1 --trials 5000 --headless --seed 1500348586 --no-logging --hfo-logging &
 sleep 15
 ./example/hand_coded_defense_agent.py &> agent1.txt &
 sleep 5
