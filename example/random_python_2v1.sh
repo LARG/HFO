c54d23500c63af4b08c42c16a8e53314f5c2b204
drallensmith <drallensmith@users.noreply.github.com>
2017-07-28 16:56:40 -0500

    Note re -x in shell scripts

--- example/random_python_2v1.sh
+++ example/random_python_2v1.sh
@@ -2,9 +2,13 @@
 
 ./bin/HFO --offense-agents=2 --defense-npcs=1 --trials 20 --headless &
 sleep 5
-python -x example/high_level_random_agent.py --port 6000 &> agent1.txt &
+
+# If wanting to test below with different python versions, add -x to avoid
+# the #!/usr/bin/env python initial line.
+
+python example/high_level_random_agent.py --port 6000 &> agent1.txt &
 sleep 5
-python -x example/high_level_random_agent.py --port 6000 &> agent2.txt &
+python example/high_level_random_agent.py --port 6000 &> agent2.txt &
 
 # The magic line
 #   $$ holds the PID for this script
