965aea3f737957eb0c3fd58599f434e9245a2169
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2017-07-28 10:17:49 -0700

    Remove numTeammates flag.

--- example/python_agents_eps_3v3.sh
+++ example/python_agents_eps_3v3.sh
@@ -3,9 +3,9 @@
 ./bin/HFO --offense-agents=2 --defense-npcs=3 --offense-npcs=1 --trials 20 --headless &
 sleep 5
 # -x is needed to skip first line - otherwise whatever default python version is will run
-python -x ./example/high_level_custom_agent.py --eps 0.2 --numTeammates=2 --port 6000 &> agent1.txt &
+python -x ./example/high_level_custom_agent.py --eps 0.2 --port 6000 &> agent1.txt &
 sleep 5
-python -x ./example/high_level_custom_agent.py --eps 0.2 --numTeammates=2 --port 6000 &> agent2.txt &
+python -x ./example/high_level_custom_agent.py --eps 0.2 --port 6000 &> agent2.txt &
 
 # The magic line
 #   $$ holds the PID for this script
